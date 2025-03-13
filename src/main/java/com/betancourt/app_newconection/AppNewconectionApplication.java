e8cce2e04e3a3d7f0610a810bae231f2eecf6b12
Wilmer Betancourt <wilmergabybet@gmail.com>
2025-03-12 20:50:39 -0500

    Create a new conection and learn this

---
+++ src/main/java/com/betancourt/app_newconection/AppNewconectionApplication.java
@@ -0,0 +1,13 @@
+package com.betancourt.app_newconection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppNewconectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppNewconectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betancourt/app_newconection/AppNewconectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betancourt.app_newconection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppNewconectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
